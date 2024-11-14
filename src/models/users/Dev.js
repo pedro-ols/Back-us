d3aa41a32ac6bf5aafb4a5b126088d552e7865a3
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2024-11-14 17:06:37 -0300

    fix: player class heritage

---
+++ src/models/users/Dev.js
@@ -0,0 +1,17 @@
+import Player from "./Players.js";
+
+class Developer extends Player {
+    constructor(name, password, currentLocal, deadOrAlive){
+        super(name, password, currentLocal, deadOrAlive)
+    }
+
+    startGame(){
+        
+    }
+    scoring(){
+
+    }
+    code(){
+
+    }
+}
