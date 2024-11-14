57d612414efbf670f615f5c037219584656e850c
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2024-11-14 16:45:39 -0300

    feat: create game class

---
+++ src/models/users/Game.js
@@ -0,0 +1,16 @@
+class Game {
+    constructor(name, activePlayers, inactivePlayers){
+        this.name = name;
+        this.activePlayers = activePlayers;
+        this.inactivePlayers = inactivePlayers;
+    }
+
+    startGame(){
+        
+    }
+    scoring(){
+
+    }
+}
+
+export default Game;
